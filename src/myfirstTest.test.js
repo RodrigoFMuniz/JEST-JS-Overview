b1d1d8f7277d248319eb6e9910ef24786da21b79
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-02-21 20:03:51 -0300

    Testing strings and reference types

--- src/myfirstTest.test.js
+++ src/myfirstTest.test.js
@@ -28,10 +28,66 @@ describe("Testing other matcher methods",()=>{
   test("Test if the variable is undefined",()=>{
     let number = null
 
-    expect(number).not.toBeDefined()
-    expect(number).toBeUndefined()
-    expect(number).not.toBeNull()
+    // expect(number).not.toBeDefined()
+    expect(number).toBeDefined()
+    // expect(number).toBeUndefined()
+    // expect(number).not.toBeNull()
     expect(number).toBeFalsy()
     expect(number).not.toBeTruthy()
   })
+
+  it("should expect zero act like zero",()=>{
+    let number = 0;
+    expect(number).toBeDefined()
+    expect(number).not.toBeUndefined()
+    expect(number).not.toBeNull()
+    expect(number).not.toBeTruthy()
+    expect(number).toBeFalsy()
+  })
+  it("Comparing numbers",()=>{
+    const n1 = 1
+    const n2 = 2
+
+    expect(n1+n2).toBeGreaterThan(2)
+    expect(n1+n2).not.toBeGreaterThan(3)
+    expect(n1+n2).toBeGreaterThanOrEqual(3)
+    // expect(n1).toBeGreaterThanOrEqual(n2)
+    expect(n1+n2).toBeLessThan(4)
+    expect(n1+n2).toBeLessThanOrEqual(3)
+    expect(n1).toBeLessThanOrEqual(n2)
+
+  })
 })
+
+describe("Testing Strings",()=>{
+  it("Should be no I in Team",()=>{
+    let string = "Teami"
+
+    expect(string).not.toMatch(/I/)
+  })
+
+  it("There is stop in Chistopher",()=>{
+    let string = "ChristoPher"
+
+    expect(string).toMatch(/stop/i)
+  })
+
+  it("Doesn't have PS4 in shopping list",()=>{
+    const shoppingList = ['PS5',"Milk","Paper towels","ps4"] 
+
+    expect(shoppingList).not.toContain('ps4')
+  })
+})
+// describe("Testing Strings",()=>{
+//   it("Should be no I in Team",()=>{
+//     let string = "TeamI"
+
+//     expect(string).not.toMatch(/I/)
+//   })
+
+//   it("There is stop in Chistopher",()=>{
+//     let string = "ChristoPher"
+
+//     expect(string).toMatch(/stop/i)
+//   })
+// })
